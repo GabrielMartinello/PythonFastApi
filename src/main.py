76785ff31fc1c00844b6d3d6b003a7cfd43bbc53
Gabriel Martinello <martinellogabriel@gmail.com>
2023-10-27 22:52:35 -0300

    Adicionado rota para envio de dados

--- src/main.py
+++ src/main.py
@@ -1,10 +1,20 @@
 import json
 from fastapi import FastAPI, Request, Response
+from datetime import datetime
 
 app = FastAPI()
 
+valores = []
+
 @app.get("/")
 def hello_world():
-    return {"message": "Hello World"}
+    return {"Teste": valores}
 
+@app.post("/cadastrar")
+async def responda_outro(request: Request):
+    body = await request.body()
+    body = dict(json.loads(body))
+    body["data_hora_criacao"] = datetime.now()
+    valores.append(body)
+    return body
 
